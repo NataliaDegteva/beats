8a3ed74d1254a4903e57926c460b6a9a5ab4af0b
NataliaDegteva <130595143+NataliaDegteva@users.noreply.github.com>
2023-07-04 18:25:52 +0300

    Add files via upload

---
+++ beats/main.js
@@ -0,0 +1,17 @@
+let burger  = document.querySelector('#burger');
+let overlay = document.querySelector('.overlay');
+let body = document.querySelector('body');
+
+let links = document.querySelectorAll('.point__link'); [1,2,3,4,5,6,7]
+
+links.forEach(function(element){
+  element.addEventListener('click' , toggleMenu);
+})
+
+function toggleMenu(){
+  burger.classList.toggle('burger--active');
+  overlay.classList.toggle('overlay--active');
+  body.classList.toggle('body--active-overlay-point');
+}
+
+burger.addEventListener('click' , toggleMenu);
