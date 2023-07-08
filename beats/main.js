f46c3b1ef14303208052db23d8d11be4d134bfc2
NataliaDegteva <130595143+NataliaDegteva@users.noreply.github.com>
2023-07-08 17:35:58 +0300

    4я неделя

--- beats/main.js
+++ beats/main.js
@@ -52,3 +52,116 @@ function toggleMenu(){
 }
 
 burger.addEventListener('click' , toggleMenu);
+
+
+
+const findBlockByAlias = alias => {
+  return $(".feedback__item").filter((ndx, item) => {
+    return $(item).attr("data-linked-with") ===alias
+  });
+};
+$(".interactive-avatar__link"). click(e=> {
+  e.preventDefault();
+
+  const $this = $(e.currentTarget);
+  const target = $this.attr("data-open");
+  const itemToShow = findBlockByAlias(target);
+  const curItem = $this.closest(".feedback__switcher-item");
+
+  itemToShow.addClass("active").siblings().removeClass("active");
+
+  curItem.addClass("interactive-avatar--active").siblings(). removeClass("interactive-avatar--active");
+});
+
+
+const slider = $(".product__list").bxSlider({
+  pager:false,
+  controls: false,
+  
+});
+
+$(".product__slider-arrow--left").click(e => {
+  e.preventDefault();
+  slider.goToPrevSlide();
+});
+
+$(".product__slider-arrow").click(e => {
+  e.preventDefault();
+  slider.goToNextSlide();
+});
+
+const validateFields = (form, fieldsArray) => {
+
+  fieldsArray.forEach(field => {
+    field.removeClass("input-error");
+    if(field.val().trim() === "") {
+      field.addClass("input-error");
+    }
+  });
+
+  const errorFields = form.find(".input-error");
+
+  return errorFields.length === 0;
+}
+
+$(".form").submit((e) => {
+  e.preventDefault();
+
+  const form = $(e.currentTarget);
+  const name = form.find("[name= 'name']");
+  const phone = form.find("[name= 'phone']");
+  const comment = form.find("[name= 'comment']");
+  const to = form.find("[name= 'to']");
+
+  const modal = $("#modal");
+  const content = modal.find(".modal__content");
+
+  modal.removeClass("error-modal");
+
+  const isValid = validateFields(form, [name,phone,comment,to]);
+
+
+  if (isValid) { 
+    $.ajax({
+      url: "https://webdev-api.loftschool.com/sendmail",
+      method: "post",
+      data: {
+        name: name.val(),
+        phone: phone.val(),
+        comment: comment.val(),
+        to: to.val()
+      },
+
+      success: data => {
+        content.text(data.message);
+       
+        $.fancybox.open( {
+          src: "#modal",
+          type: "inline"
+        });
+      },
+      error: data => {
+        const message= data.responseJSON.message;
+        content.text(message);
+        modal.addClass("error-modal");
+        $.fancybox.open( {
+          src: "#modal",
+          type: "inline"
+        });
+      }
+    });
+  };
+
+
+
+ /* $.fancybox.open( {
+    src: "#modal",
+    type: "inline"
+  });*/
+});
+
+$(".app-submit-button").click(e=> {
+  e.preventDefault();
+
+  $.fancybox.close();
+});
